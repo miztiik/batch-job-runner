de877e84b4857b22f7fe9cbb947f2e824deb0714
Mystique <miztiik@users.noreply.github.com>
2020-05-07 20:04:05 +0200

    epoch_time_support

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -2,5 +2,5 @@
 
 echo -e '{"begin_job":"True"}'
 echo -e "{'batch_server_host':`hostname`}"
-echo -e "{'job_date':`date`}"
+echo -e "{'job_date':`date +%s`}"
 echo -e '{"ending_job":"True","job_completed_sucessfully":"True"}'
